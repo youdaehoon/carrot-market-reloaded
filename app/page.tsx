1b2377cb18b26d133698a6b00dd918c437a4adf7
유대훈 <sjssmsqkqh1@gnaver.com>
2024-06-05 10:44:17 +0900

    #3.7 - modifier 공부 - peer를 통해 형제 선택자 - has를 통해 자식의 상태를 선택

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,37 @@
 export default function Home() {
   return (
     <main
-      className="bg-gray-100 h-screen flex items-center justify-center p-5 
+      className="bg-gray-100 h-screen 
+      sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100
+      flex items-center justify-center p-5 
     dark:bg-gray-700
     "
     >
       <div
-        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm
-      dark:bg-gray-600
+        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2
+      dark:bg-gray-600 md:flex-row *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100  has-[:invalid]:ring
       "
       >
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col ">
-            <span
-              className="text-gray-600 font-semibold -mb-1
-            dark:text-gray-300
-            "
-            >
-              In transit
-            </span>
-            <span
-              className="text-4xl font-semibold
-            dark:text-white
-            "
-            >
-              Coolblue
-            </span>
-          </div>
-          <div className="size-12 rounded-full  bg-orange-400" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span
-            className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full transition
-           hover:bg-green-500  hover:scale-125
-           "
-          >
-            Today
-          </span>
-          <span className="dark:text-gray-100">9:103~10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 w-full h-2 rounded-full absolute" />
-          <div className="bg-green-400 w-2/3 h-2 rounded-full absolute" />
-        </div>
-        <div
-          className="flex justify-between items-center mt-5 text-gray-600 font-medium
-        dark:text-gray-300
+        <input
+          className="w-full rounded-full h-10 bg-gray-200 pl-5  ring
+        ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow
+        placeholder:drop-shadow invalid:focus:ring-red-500 peer
+        "
+          type="text"
+          placeholder="Email address"
+          required
+        ></input>
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          {" "}
+          Email is required.
+        </span>
+        <button
+          className="bg-black text-white py-2 rounded-full font-medium
+        active:scale-90 md:px-10  transition-transform  peer-invalid:bg-red-100 peer-required:bg-green-500
         "
         >
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span
-            className="text-gray-400
-               dark:text-gray-500
-          "
-          >
-            Delivered
-          </span>
-        </div>
+          Login
+        </button>
       </div>
     </main>
   );
